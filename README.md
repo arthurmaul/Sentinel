48cc63be93ab51128911bc00221fbf5275a615d3
arthurmauliiengineering <arthurmauliiengineering@gmail.com>
2024-06-28 15:55:54 -0500

    feature: routines running on an interval, call a given task each iteration

--- README.md
+++ README.md
@@ -9,45 +9,6 @@ A simple background worker for data orchestration and task handling in golang.
 - [ ] Put a file on disk
 - [ ] Pull a file from disk
 
-## Architecture
-```
-.
-├── main.go
-├── internal/
-│   ├── core/
-│   └── rest/
-│   ├── model/
-├── go.mod
-└── go.sum
-```
-
-The  main.go file is the base of the project and will only import internal and will not import any other files.
-
-The `interal/core` folder holds tasks, routines, and sensors. These will import the models in `internal/core` .
-
-The `interal/rest` folder holds the handler methods for any http functionality and any data local to those operations. They will import `internal/model` .
-
-The `internal/model` folder holds the data representation types and structures. these will be imported by everything else, and have no external dependencies. There should be *one model per file*.
-
-`internal/core` and `internal/rest` import `internal/model` , which will not import anything else.
-
-This can be visualized as a series of progressively deeper layers:
-
-```mermaid
-graph TD
-    A(server)
-    B(rest)
-    C(core)
-    D(model)
-
-    A--> B --> D
-    A --> C --> D
-```
-
-This avoids any potential for cyclic dependencies and orients the api around the domain data.
-
-This data should be defined first, and then the corresponding functionality should be created to manipulate that data.
-
 # Documentation
-See the getting started guide [here](docs/startup.md) for further direction.
+See the getting started guide [here](docs/quickstart.md) for further direction.
 
--- main.go
+++ main.go
@@ -1,46 +1,90 @@
 package main
 
 import (
+    "time"
     "log"
+    "errors"
     "net/http"
 )
 
-func handleTask(response http.ResponseWriter, request *http.Request) {
-    method  := request.Method
-    taskID  := request.PathValue("taskid")
-    payload := make(map[string][]string)
-
-    // Determine method type and extract data.
-    switch method {
-    case "GET":
-        payload = request.URL.Query()
-    }
-
-    // Call proper function with payload data.
-    switch taskID {
-    case "polkadots":
-        polkadots(payload)
-    }
-
-    log.Println(method, "method targeting task", taskID)
-    log.Println("payload of ", payload)
-}
-
-func polkadots(data any){
-    log.Println("!!!!!!!")
-    log.Println(data)
-}
+const base = "http://"
+const url  = "localhost:"
+const port = "8080"
 
 func main() {
     router := http.NewServeMux()
-    router.HandleFunc("GET /tasks/{taskid}", handleTask)
-    router.HandleFunc("POST /tasks/{taskid}", handleTask)
+    router.HandleFunc("GET /{taskid}", handleGetRequest)
+    router.HandleFunc("POST /{taskid}", handlePostRequest)
+
     server := http.Server{
-        Addr: "localhost:8080",
+        Addr: url + port,
         Handler: router,
     }
 
-    log.Println("Server Created")
-    log.Println("Running on Port: 8080")
-    server.ListenAndServe()
+    go exampleRoutineA()
+    go exampleRoutineB()
+
+    err := server.ListenAndServe()
+    if err != nil {
+        if !errors.Is(err, http.ErrServerClosed) {
+            log.Printf("error running http server: %s\n", err)
+        }
+    }
 }
+
+func handleGetRequest(response http.ResponseWriter, request *http.Request) {
+    taskId  := request.PathValue("taskid")
+    payload := request.URL.Query()
+
+    log.Println(
+        "REQUEST",
+        "\n\tmethod:",  request.Method,
+        "\n\ttarget:",  taskId,
+        "\n\tpayload:", payload,
+    )
+
+    triggerTask(taskId, payload)
+
+}
+
+func handlePostRequest(response http.ResponseWriter, request *http.Request) {
+    // TODO: determine and type post data and delegate accordingly
+}
+
+func triggerTask(id string, payload any) {
+    switch id {
+    case "exampleTaskA":
+        exampleTaskA(payload)
+    case "exampleTaskB":
+        exampleTaskB(payload)
+    default:
+        log.Println("Task not found.")
+    }
+}
+
+func exampleTaskA(data any) error {
+    log.Println("Task A running ...")
+    return nil
+}
+
+func exampleTaskB(data any) error {
+    log.Println("Task B running ...")
+    return nil
+}
+
+func exampleRoutineA() {
+    ticker := time.NewTicker(5 * time.Second)
+    for {
+        <-ticker.C
+        http.Get(base + url + port + "/exampleTaskA?hello=world_a")
+    }
+}
+
+func exampleRoutineB() {
+    ticker := time.NewTicker(1 * time.Minute)
+    for {
+        <-ticker.C
+        http.Get(base + url + port + "/exampleTaskB?testing=routine_b")
+    }
+}
+
