a8c2092c18d57c8ab2bfe2301f871e1894d7304d
arthurmauliiengineering <arthurmauliiengineering@gmail.com>
2024-06-27 20:03:02 -0500

    refactor: removed unnecessary abstractions and cleaned up api

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
-module github.com/arthurmaul/goworker.git
+module github.com/arthurmaul/goworker
 
 go 1.22.3
+
+require (
+	github.com/americanexpress/baton v0.0.0-20200510181441-576339041346 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/d4l3k/go-pry v0.0.0-20230221054152-cca3eb982836 // indirect
+	github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.55.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+    "log"
+    "net/http"
+)
+
+func handleTask(response http.ResponseWriter, request *http.Request) {
+    method  := request.Method
+    taskID  := request.PathValue("taskid")
+    payload := make(map[string][]string)
+
+    // Determine method type and extract data.
+    switch method {
+    case "GET":
+        payload = request.URL.Query()
+    }
+
+    // Call proper function with payload data.
+    switch taskID {
+    case "polkadots":
+        polkadots(payload)
+    }
+
+    log.Println(method, "method targeting task", taskID)
+    log.Println("payload of ", payload)
+}
+
+func polkadots(data any){
+    log.Println("!!!!!!!")
+    log.Println(data)
+}
 
 func main() {
-    fmt.Println("hello world")    
+    router := http.NewServeMux()
+    router.HandleFunc("GET /tasks/{taskid}", handleTask)
+    router.HandleFunc("POST /tasks/{taskid}", handleTask)
+    server := http.Server{
+        Addr: "localhost:8080",
+        Handler: router,
+    }
+
+    log.Println("Server Created")
+    log.Println("Running on Port: 8080")
+    server.ListenAndServe()
 }
